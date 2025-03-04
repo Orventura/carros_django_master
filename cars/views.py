5b75d450ff990334f988b4cee3d876ff677e0441
roberto <robertoventura.rvs@gmail.com>
2025-03-04 18:32:31 -0400

    estilização inclusa na página cars

--- cars/views.py
+++ cars/views.py
@@ -2,7 +2,14 @@ from django.shortcuts import render
 from cars.models import Car
 
 def car_view(request):
-    #cars = Car.objects.all()
-    cars = Car.objects.filter(brand__name='Fiat')
+    cars = Car.objects.all().order_by('brand__name', 'model')
+    print(request.GET.get('search'))
+
+    search = request.GET.get('search')
+
+    #cars = Car.objects.filter(brand__name='Chevrolet')
+    if search:    
+        cars = Car.objects.filter(model__icontains=search).order_by('brand__name', 'model')
+
 
     return render(request=request, template_name='cars.html', context={'cars': cars})
