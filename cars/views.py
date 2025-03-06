1b5d714e1d9e230a0d2d9c1c267996df058c453b
roberto <robertoventura.rvs@gmail.com>
2025-03-05 21:20:46 -0400

    função new_car implementada

--- cars/views.py
+++ cars/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from cars.models import Car
+from cars.forms import Carform
+
 
 def car_view(request):
     cars = Car.objects.all().order_by('brand__name', 'model')
@@ -13,3 +15,7 @@ def car_view(request):
 
 
     return render(request=request, template_name='cars.html', context={'cars': cars})
+
+def new_car_view(request):
+    new_car_form = Carform()
+    return render(request, 'new_car.html', { 'new_car_form': new_car_form })
